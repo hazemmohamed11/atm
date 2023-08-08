ae7e7105582e6541f90c1771b81fc35014c490b8
hazemmohamed11 <37817913+hazemmohamed11@users.noreply.github.com>
2023-08-08 07:11:52 +0300

    Delete resources/views directory

--- resources/views/account/balance.blade.php
+++
@@ -1,1 +0,0 @@
-<!DOCTYPE html> <html> <head>     <title>Account Balance</title> </head> <body>     <h1>Account Balance</h1>     <p>{{ $balance }}</p> </body> </html>
--- resources/views/user/account_options.blade.php
+++
@@ -1,9 +0,0 @@
-
-
-
-
-<h1>Account Options</h1>
-   <ul>
-       <li><a href="{{ route('account.balance', $account) }}">Check Account Balance</a></li>
-       <li><a href="{{ route('account.withdraw', $account) }}">Withdraw Cash</a></li>
-   </ul>
